b58c21f41896d2823bfd8caaac013fc1fe876c90
Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2020-04-07 16:49:04 +0200

    Add socket.io

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 //On appelle tous les modules dont on a besoin
-var express = require('express')
-var app = express()
-var server = require('http').createServer(app)
-var io = require('socket.io').listen(server)
-var ent = require('ent')
-var bodyParser = require('body-parser')
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var ent = require('ent');
+var bodyParser = require('body-parser');
 var cors = require('cors');
 
 //On dit à notre application d'utiliser nos modules
@@ -20,27 +19,55 @@ app.post('/login', (req, res, next) => {
 
     //Si le pincode (contenu dans le body) est '1234' on retourne le status 200 (ok)
     if (req.body.pincode == "1234") {
-        return res.status(200).json({ status: true })
-
-    //Sinon, on retourne le status 404 (pas ok)
-    } else {
-        return res.status(404).json({ status: false })
-    }
+        return res.status(200).json({ status: true });
+        
+    //Sinon, on retourne le status 402 (pas ok)
+} else {
+    return res.status(402).json({ status: false });
+}
 })
 
 //On définit nos routes
 app.get('/', function(req, res) {
-    res.render('index.ejs')
-})
+    res.render('index.ejs');
+});
 app.get('/chat', function(req, res) {
-    res.render('chat.ejs')
-})
+    res.render('chat.ejs');
+});
 
 //Si on va sur une page non définie sur le port de node, on écrit "Page introuvable"
 app.use(function(req, res, next) {
-    res.setHeader('Content-type', 'text/html')
-    res.status(404).send('Page introuvable !')
-})
+    res.setHeader('Content-type', 'text/html');
+    res.status(404).send('Page introuvable !');
+});
+
+
+
+
+
+// IO
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+
+    socket.on('pseudo', (pseudo)=>{
+        socket.pseudo = pseudo;
+
+        socket.broadcast.emit('newUser', pseudo);
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('quitUser', socket.pseudo);
+    });
+
+    socket.on('newMessage', (message)=> {
+        socket.broadcast.emit('newMessageAll', {message: message, pseudo: socket.pseudo});
+    });
+
+});
+
+
 
 //On dit à Node de se lancer sur le port 8080
-server.listen(8080, () => console.log('Server started at port : 8080'))
+server.listen(8080, () => console.log('Server started at port : 8080'));
